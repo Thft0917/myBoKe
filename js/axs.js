264405124998da1a2c3d63bf4d77b1b306decdd3
Thft0917 <51598631+Thft0917@users.noreply.github.com>
2024-10-20 15:30:53 +0800

    Update axs.js

--- js/axs.js
+++ js/axs.js
@@ -1,7 +1,7 @@
 
 // 创建 axios 实例
 const service = axios.create({
-  baseURL: 'https://118.31.42.182:3000', // 接口的基础地址，可以配置为环境变量
+  baseURL: 'http://118.31.42.182:3000', // 接口的基础地址，可以配置为环境变量
   timeout: 5000, // 请求超时的时间
 });
 
